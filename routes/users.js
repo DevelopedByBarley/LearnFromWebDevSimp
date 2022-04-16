832160018affdab022cef7180b8d25da78dfc331
Árpád <developedbybarley@gmail.com>
2022-04-16 10:00:30 +0200

    Use Form and redirect()

--- routes/users.js
+++ routes/users.js
@@ -1,22 +1,38 @@
 const express = require('express');
-const router = express.Router(); 
-router.use(logger); 
+const router = express.Router();
+router.use(logger);
 
 router.get('/', (req, res) => {
-  res.send('dat was get')
+  res.send('dat was get on users')
 })
 
-router.get('/new', (req, res) => { 
-  res.send('usersNewForm')
+router.get('/new', (req, res) => { // 1 Létrehozunk egy formot a views/users/new.ejs-en belül
+  res.render("users/new", { firstName: 'Test' }) // 2 Amelyet kirenderelünk és átadjuk neki a firsName: 'Test' value-t ami a form input value-ként fog funkcionálni
 })
 
-router.get('/new/first', (req, res) => { 
-  res.send('usersNewForm-first')
+router.post('/', (req, res) => { // ezt elküldjük post utvonalra
+  const isValid = true; // Megvizsgálunk egy if else ágat
+  if(isValid) { // Ami ha igaz
+    users.push({ firstName: req.body.firstName}); // users tömbhöz pussoljuk a req.body.firstName kulcsát
+    res.redirect(`/users/${users.length - 1}`) // Majd átirányitunk egy másik url-re ami a /users/userId lesz userId-nak átadva az users.lenght - 1 -et igy megkava az utolsó elemet amit hozzáadtunk a tömbhöz
+  } else {
+    console.log("error"); // Más esetben
+    res.render(`users/new`, {firstName: req.body.firstName}) // Kirendereljük újra az users.new-ot nem átirányitunk mert annak nem tudunk átadni body-t
+  }
+  res.send('Hi')
 })
 
-router.route("/:userId") 
+
+
+
+
+
+
+
+
+router.route("/:userId")
   .get((req, res) => {
-    console.log(req.user); 
+    console.log(req.user);
     res.send(`dat was get on ${req.params.userId}`);
   })
   .post((req, res) => {
@@ -29,16 +45,16 @@ router.route("/:userId")
     res.send(`dat was put on ${req.params.userId}`);
   })
 
-const users = [{ name: "kyle" }, { name: "sally" }]; 
+const users = [{ name: "kyle" }, { name: "sally" }];
 router.param('userId', (req, res, next, id) => {
   console.log(id)
   req.user = users[id];
   next();
 })
 
-function logger(req, res, next) { 
-  console.log(req.originalUrl) 
-  next(); 
+function logger(req, res, next) {
+  console.log(req.originalUrl)
+  next();
 }
 
 
--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static("public")) // kiszolgáljuk a teljes public mappát, majd ha még a /test/tt.html-t példaként beirjuk, azt is kiszolgálja!
+app.use(express.static("public"))
+app.use(express.urlencoded({extended: true}))
 app.set('view engine', 'ejs') 
 
-app.get('/', (req, res) => {
-  
-})
 
 const userRouter = require('./routes/users'); 
 
