d75d2dd383aafb649e81874c91798a812ab79649
Árpád <developedbybarley@gmail.com>
2022-04-16 09:22:02 +0200

    Middleware functions!

--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,22 @@
 const express = require('express');
-const router = express.Router(); // Behuzunk egy ROUTERT hogy külön kis appot tudjuk késziteni routernek amit késöbb beágyazhatunk
+const router = express.Router(); 
+router.use(logger); // 4. Hogy a logger middleware-t elérjük , használnunk is kell a routeren belül
 
-router.get('/', (req, res) => { // Mivel hogy késöbb usersként fogjuk behuzni a mainbe ezért nem kell /usersnek irni hanem csak /-nek
+router.get('/', (req, res) => {
   res.send('dat was get')
 })
 
-router.get('/new', (req, res) => { // Mivel hogy késöbb usersként fogjuk behuzni a mainbe ezért nem kell /usersnek/new irni hanem csak /new-nek
+router.get('/new', (req, res) => { 
   res.send('usersNewForm')
 })
 
-router.get('/new/first', (req, res) => { // Mivel hogy késöbb usersként fogjuk behuzni a mainbe ezért nem kell /usersnek/new irni hanem csak /new-nek
+router.get('/new/first', (req, res) => { 
   res.send('usersNewForm-first')
 })
 
-router.route("/:userId") // Létrehozunk egy router.route-t a könnyebb szintaxis érdekében
+router.route("/:userId") 
   .get((req, res) => {
-    console.log(req.user); //3. Innentől már a routeren belül bárhol elérhetjük a request user kulcsát, ami az id
+    console.log(req.user); 
     res.send(`dat was get on ${req.params.userId}`);
   })
   .post((req, res) => {
@@ -28,12 +29,18 @@ router.route("/:userId") // Létrehozunk egy router.route-t a könnyebb szintaxi
     res.send(`dat was put on ${req.params.userId}`);
   })
 
-const users = [{ name: "kyle" }, { name: "sally" }]; // 1Létrehozunk egy users tömböt
-router.param('userId', (req, res, next, id) => { // 2melyben a router param értékének átadjuk az userId-t -> id paraméternek, majd a /válasznak/ req.user-nek átadjuk a users tömb id indexét
+const users = [{ name: "kyle" }, { name: "sally" }]; 
+router.param('userId', (req, res, next, id) => {
   console.log(id)
   req.user = users[id];
   next();
 })
+
+function logger(req, res, next) { // 1 Létrehozunk egy middleware-t ami a res és req közötti állapotot tudja reprezentálni és müveleteket végrehajtani
+  console.log(req.originalUrl) // 2 Kiiratjuk benne az url-t 
+  next(); // 3 majd a kontrollt átpasszoljuk a következő sor kódoknak
+  // Mivel ez is úgy müködik mint egy változó igy scope-ja van, mivel ezt az users.js routerjében deklaráltuk, ezért csak azon belül fog müködni
+}
 
 // 23:00
 
--- server.js
+++ server.js
@@ -5,11 +5,13 @@ app.set('view engine', 'ejs') // Behúzzuk az EJS-t
 
 app.get('/', (req, res) => {
   // res.status(500).json({ message: "Error"})
-res.render('index', { text: "World" }) // Ki rendereljük az EJS-t átadva neki paramétereket kulcs értékpárokként 
+  res.render('index', { text: "World" }) // Ki rendereljük az EJS-t átadva neki paramétereket kulcs értékpárokként 
 })
 
 const userRouter = require('./routes/users'); // behuzzuk az exportált filet
 
 app.use('/users', userRouter); // Majd használatra birjuk első paraméterként az itteni utvonallal amit átpasszolunk , másodikként pedig a routerböl kapott utvonallal, igy összeáll a teljes utvonal
+
+
 
 app.listen(3000);
