13bc913b072934e1030e2f9f92a1ddc4d9ca3dd4
Árpád <developedbybarley@gmail.com>
2022-04-16 09:27:19 +0200

    Serving static files!

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router(); 
-router.use(logger); // 4. Hogy a logger middleware-t elérjük , használnunk is kell a routeren belül
+router.use(logger); 
 
 router.get('/', (req, res) => {
   res.send('dat was get')
@@ -36,15 +36,14 @@ router.param('userId', (req, res, next, id) => {
   next();
 })
 
-function logger(req, res, next) { // 1 Létrehozunk egy middleware-t ami a res és req közötti állapotot tudja reprezentálni és müveleteket végrehajtani
-  console.log(req.originalUrl) // 2 Kiiratjuk benne az url-t 
-  next(); // 3 majd a kontrollt átpasszoljuk a következő sor kódoknak
-  // Mivel ez is úgy müködik mint egy változó igy scope-ja van, mivel ezt az users.js routerjében deklaráltuk, ezért csak azon belül fog müködni
+function logger(req, res, next) { 
+  console.log(req.originalUrl) 
+  next(); 
 }
 
-// 23:00
 
 
 
 
-module.exports = router; // Exportáljuk hogy a mainbe felhasználgassuk
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const app = express();
 
-app.set('view engine', 'ejs') // Behúzzuk az EJS-t
+app.use(express.static("public")) // kiszolgáljuk a teljes public mappát, majd ha még a /test/tt.html-t példaként beirjuk, azt is kiszolgálja!
+app.set('view engine', 'ejs') 
 
 app.get('/', (req, res) => {
-  // res.status(500).json({ message: "Error"})
-  res.render('index', { text: "World" }) // Ki rendereljük az EJS-t átadva neki paramétereket kulcs értékpárokként 
+  
 })
 
-const userRouter = require('./routes/users'); // behuzzuk az exportált filet
+const userRouter = require('./routes/users'); 
 
-app.use('/users', userRouter); // Majd használatra birjuk első paraméterként az itteni utvonallal amit átpasszolunk , másodikként pedig a routerböl kapott utvonallal, igy összeáll a teljes utvonal
+app.use('/users', userRouter);
 
 
 
